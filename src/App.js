450adad7e9f65be13c8543ec954eb9c840167ae1
Dean Kerr <dean.kerr@gmail.com>
2022-01-14 22:11:23 +1100

    Add react-simple-keyboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css'
 
+import Keyboard from 'react-simple-keyboard'
+import 'react-simple-keyboard/build/css/index.css'
+
+
 function App() {
   return (
     <div className="App">
@@ -14,110 +18,109 @@ function App() {
           <div className="cellContainer">
 
 
-
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-
-
-
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
 
 
 
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
 
 
 
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
 
 
 
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
 
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
-              <div className="cell">
-                A
-              </div>
+
+
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
+            <div className="cell">
+              A
+            </div>
 
 
           </div>
@@ -125,7 +128,17 @@ function App() {
         </div>
 
         <div className="keyboard">
-          keyboard goes here
+          <Keyboard
+            layout={
+              {
+                default: [
+                  'Q W E R T Y U I O P',
+                  'A S D F G H J K L',
+                  '{enter} Z X C V B N M {bksp}',
+                ]
+              }
+            }
+          />
         </div>
       </div>
 
