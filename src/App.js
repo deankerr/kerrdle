72c5c22d32ad1817e6576715cbc3f8d597ab84d6
Dean Kerr <dean.kerr@gmail.com>
2022-01-15 14:59:32 +1100

    Add programatically generated board state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import './App.css'
+import { useState } from 'react'
 
 import Keyboard from 'react-simple-keyboard'
 import 'react-simple-keyboard/build/css/index.css'
 
 import Grid from './components/Grid'
 
+
+// Initial board generation
+const WORD_LENGTH = 5
+const MAX_ATTEMPTS = 6
+
+let INITIAL_BOARD = []
+for (let i = 0; i < MAX_ATTEMPTS; i++) {
+  let row = []
+  for (let j = 0; j < WORD_LENGTH; j++) {
+    row.push('')
+  }
+  INITIAL_BOARD.push(row)
+}
+
+
 function App() {
+
+  const [ gameOver, setGameOver ] = useState(false)
+
+  const [ board, setBoard ] = useState(INITIAL_BOARD)
+  const [ attempt, setAttempt ] = useState(0)
+
+  const [ word, setWord ] = useState("TIGER") // TODO: Pull random world from list
+
   return (
     <div className="App">
 
       <div className="game">
+
         <header>
           KERRDLE
         </header>
 
         <div className="gameArea">
 
-          <Grid />
+          <Grid board={board} />
 
         </div>
 
+
         <div className="keyboard">
+
           <Keyboard
             layout={
               {
@@ -38,7 +65,9 @@ function App() {
               }
             }
           />
+
         </div>
+
       </div>
 
     </div>
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,23 +1,25 @@
-const GRID_SIZE = 30;
+export default function Grid(props) {
 
-export default function Grid() {
+  const board = props.board
 
   let grid = []
-  for (let i = 0; i < (GRID_SIZE); i++) {
-    grid.push(
-      <div className="cell" id={i} key={i}>
-        A
-      </div>
-    )
-  }
+  board.forEach(row => {
+    row.forEach(cell => {
+      grid.push(
+        <div className="cell" >
+          {cell}
+        </div>
+      )
+    })
+  })
 
 
   return (
     <div className="cellContainer">
 
-    {
-      grid.map(e => e)
-    }
+      {
+        grid.map(e => e)
+      }
 
     </div>
 
