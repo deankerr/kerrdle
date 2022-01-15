1822f9e0e5d65eadd63db92a1e95f3f146c6704e
Dean Kerr <dean.kerr@gmail.com>
2022-01-15 15:56:22 +1100

    Re-add key to cells

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,13 +3,15 @@ export default function Grid(props) {
   const board = props.board
 
   let grid = []
+  let id = 0
   board.forEach(row => {
     row.forEach(cell => {
       grid.push(
-        <div className="cell" >
+        <div className="cell" key={id}>
           {cell}
         </div>
       )
+      id++
     })
   })
 
